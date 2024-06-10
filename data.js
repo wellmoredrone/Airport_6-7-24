2057aea2c9d1c737ca699e2bc79da2a17f1397de
wellmoredrone <85592388+wellmoredrone@users.noreply.github.com>
2024-06-10 14:54:36 -0400

    INIT

---
+++ data.js
@@ -0,0 +1,731 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-turkey-pen-south",
+      "name": "Turkey Pen South",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.479402745666162,
+        "pitch": 0.3861845098013781,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.599523213455596,
+          "pitch": -0.030128964402901914,
+          "rotation": 0,
+          "target": "2-big-rock-branch"
+        },
+        {
+          "yaw": 2.324178318132976,
+          "pitch": -0.04108448288247857,
+          "rotation": 0,
+          "target": "7-west-runway"
+        },
+        {
+          "yaw": 2.4480909723591147,
+          "pitch": -0.039665796197216,
+          "rotation": 0,
+          "target": "6-runway-grading"
+        },
+        {
+          "yaw": 1.9625394135598064,
+          "pitch": -0.03285175091727588,
+          "rotation": 0,
+          "target": "8-work-area"
+        },
+        {
+          "yaw": 3.073137714652482,
+          "pitch": -0.03132204184919374,
+          "rotation": 0,
+          "target": "3-north-of-runway"
+        },
+        {
+          "yaw": -1.4138956937571763,
+          "pitch": -0.04875267688906959,
+          "rotation": 0,
+          "target": "1-turkey-pen-hollow"
+        },
+        {
+          "yaw": -2.4952798692626814,
+          "pitch": -0.054977622431030326,
+          "rotation": 0,
+          "target": "4-cripple-creek"
+        },
+        {
+          "yaw": 2.6798664874590736,
+          "pitch": -0.05552089722583453,
+          "rotation": 0,
+          "target": "5-runway-view-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-turkey-pen-hollow",
+      "name": "Turkey Pen Hollow",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.09646056159656169,
+        "pitch": 0.41748084418145304,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5606424619101205,
+          "pitch": -0.036217616020667975,
+          "rotation": 0,
+          "target": "2-big-rock-branch"
+        },
+        {
+          "yaw": -0.5155158285397974,
+          "pitch": 0.05746107971962644,
+          "rotation": 0,
+          "target": "0-turkey-pen-south"
+        },
+        {
+          "yaw": -0.3005755727590973,
+          "pitch": -0.028547274265349643,
+          "rotation": 0,
+          "target": "8-work-area"
+        },
+        {
+          "yaw": 0.018970964831208192,
+          "pitch": -0.0546923636633192,
+          "rotation": 0,
+          "target": "7-west-runway"
+        },
+        {
+          "yaw": -0.0829902455248206,
+          "pitch": -0.059326944995778064,
+          "rotation": 0,
+          "target": "6-runway-grading"
+        },
+        {
+          "yaw": 0.020928648206002265,
+          "pitch": -0.1637156768999617,
+          "rotation": 0,
+          "target": "5-runway-view-"
+        },
+        {
+          "yaw": 0.7914787653247384,
+          "pitch": -0.020913094013081945,
+          "rotation": 0,
+          "target": "4-cripple-creek"
+        },
+        {
+          "yaw": 0.5012648155393542,
+          "pitch": -0.04372329333005176,
+          "rotation": 0,
+          "target": "3-north-of-runway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-big-rock-branch",
+      "name": "Big Rock Branch",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.05263691394902992,
+        "pitch": 0.5791954549375369,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5548220786671827,
+          "pitch": 0.2093293276846815,
+          "rotation": 0,
+          "target": "8-work-area"
+        },
+        {
+          "yaw": -0.5694033083156498,
+          "pitch": -0.05023813588592496,
+          "rotation": 0,
+          "target": "6-runway-grading"
+        },
+        {
+          "yaw": -1.1790120138199,
+          "pitch": -0.060933036584533085,
+          "rotation": 0,
+          "target": "7-west-runway"
+        },
+        {
+          "yaw": -0.19263155752718752,
+          "pitch": -0.03475245427784657,
+          "rotation": 0,
+          "target": "3-north-of-runway"
+        },
+        {
+          "yaw": 0.02886867702259721,
+          "pitch": -0.03514808405257064,
+          "rotation": 0,
+          "target": "5-runway-view-"
+        },
+        {
+          "yaw": 0.30919912948533046,
+          "pitch": -0.061164196379653646,
+          "rotation": 0,
+          "target": "4-cripple-creek"
+        },
+        {
+          "yaw": 0.9221712349294062,
+          "pitch": -0.05411019142031748,
+          "rotation": 0,
+          "target": "1-turkey-pen-hollow"
+        },
+        {
+          "yaw": 1.0668379846019445,
+          "pitch": -0.019713511644065207,
+          "rotation": 0,
+          "target": "0-turkey-pen-south"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-north-of-runway",
+      "name": "North of Runway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.22578278621424097,
+        "pitch": 0.353534888078757,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8614866431425732,
+          "pitch": -0.04298280067931515,
+          "rotation": 0,
+          "target": "0-turkey-pen-south"
+        },
+        {
+          "yaw": -1.0998516286114164,
+          "pitch": -0.003959966973599904,
+          "rotation": 0,
+          "target": "1-turkey-pen-hollow"
+        },
+        {
+          "yaw": -1.430211053776091,
+          "pitch": -0.004126943352359191,
+          "rotation": 0,
+          "target": "4-cripple-creek"
+        },
+        {
+          "yaw": -0.23269869718895997,
+          "pitch": -0.061189841870366024,
+          "rotation": 0,
+          "target": "2-big-rock-branch"
+        },
+        {
+          "yaw": 0.14805893627955413,
+          "pitch": -0.05357466400792532,
+          "rotation": 0,
+          "target": "8-work-area"
+        },
+        {
+          "yaw": 0.35429560161501783,
+          "pitch": -0.06757841485937988,
+          "rotation": 0,
+          "target": "6-runway-grading"
+        },
+        {
+          "yaw": 0.7895397368692141,
+          "pitch": -0.06328181710113867,
+          "rotation": 0,
+          "target": "7-west-runway"
+        },
+        {
+          "yaw": -0.5445687032778359,
+          "pitch": -0.06401401695440612,
+          "rotation": 0,
+          "target": "5-runway-view-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-cripple-creek",
+      "name": "Cripple Creek",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.286733951932149,
+        "pitch": 0.40995177986472164,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6062481201541274,
+          "pitch": -0.03627198309512636,
+          "rotation": 0,
+          "target": "1-turkey-pen-hollow"
+        },
+        {
+          "yaw": -0.29785741071509975,
+          "pitch": -0.045515490793231805,
+          "rotation": 0,
+          "target": "0-turkey-pen-south"
+        },
+        {
+          "yaw": 0.29805174373705334,
+          "pitch": -0.04517502319772859,
+          "rotation": 0,
+          "target": "2-big-rock-branch"
+        },
+        {
+          "yaw": 0.12335251938584157,
+          "pitch": -0.04466399578568492,
+          "rotation": 0,
+          "target": "5-runway-view-"
+        },
+        {
+          "yaw": 0.7858834951306424,
+          "pitch": -0.04921511107381349,
+          "rotation": 0,
+          "target": "8-work-area"
+        },
+        {
+          "yaw": 1.2516874408753669,
+          "pitch": -0.04704677432857807,
+          "rotation": 0,
+          "target": "7-west-runway"
+        },
+        {
+          "yaw": 1.0594777613817197,
+          "pitch": -0.04526422902236682,
+          "rotation": 0,
+          "target": "6-runway-grading"
+        },
+        {
+          "yaw": 1.6003793207065744,
+          "pitch": -0.036619439775389395,
+          "rotation": 0,
+          "target": "3-north-of-runway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-runway-view-",
+      "name": "Runway View ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.758296088279053,
+        "pitch": 0.5200468634040369,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2093212797543575,
+          "pitch": -0.07437573967638933,
+          "rotation": 0,
+          "target": "7-west-runway"
+        },
+        {
+          "yaw": 2.112566115394319,
+          "pitch": -0.07935679179555954,
+          "rotation": 0,
+          "target": "6-runway-grading"
+        },
+        {
+          "yaw": 1.1549255475396372,
+          "pitch": -0.05395027531091401,
+          "rotation": 0,
+          "target": "8-work-area"
+        },
+        {
+          "yaw": -2.7582420700069097,
+          "pitch": -0.06582050786224514,
+          "rotation": 0,
+          "target": "3-north-of-runway"
+        },
+        {
+          "yaw": -1.5863902672753234,
+          "pitch": -0.06415083231341612,
+          "rotation": 0,
+          "target": "4-cripple-creek"
+        },
+        {
+          "yaw": -1.087707654814313,
+          "pitch": -0.05312488985263997,
+          "rotation": 0,
+          "target": "1-turkey-pen-hollow"
+        },
+        {
+          "yaw": -0.7018988594930704,
+          "pitch": -0.05488924319403665,
+          "rotation": 0,
+          "target": "0-turkey-pen-south"
+        },
+        {
+          "yaw": 0.46218157239407986,
+          "pitch": -0.06821667253702302,
+          "rotation": 0,
+          "target": "2-big-rock-branch"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-runway-grading",
+      "name": "Runway Grading",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.1674766626182702,
+        "pitch": 0.5885979785566633,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.126321159511022,
+          "pitch": -0.07850943261773047,
+          "rotation": 0,
+          "target": "3-north-of-runway"
+        },
+        {
+          "yaw": -1.2204151295418058,
+          "pitch": -0.17317722609435116,
+          "rotation": 0,
+          "target": "1-turkey-pen-hollow"
+        },
+        {
+          "yaw": -0.8718292323834618,
+          "pitch": -0.12270808406873535,
+          "rotation": 0,
+          "target": "0-turkey-pen-south"
+        },
+        {
+          "yaw": -1.1050130559733198,
+          "pitch": -0.13868510530263478,
+          "rotation": 0,
+          "target": "5-runway-view-"
+        },
+        {
+          "yaw": -1.544046369847404,
+          "pitch": -0.08390903726697374,
+          "rotation": 0,
+          "target": "4-cripple-creek"
+        },
+        {
+          "yaw": -0.44343516242782677,
+          "pitch": -0.0710365969566702,
+          "rotation": 0,
+          "target": "2-big-rock-branch"
+        },
+        {
+          "yaw": 0.2723552502996309,
+          "pitch": -0.07114326599343102,
+          "rotation": 0,
+          "target": "8-work-area"
+        },
+        {
+          "yaw": 1.789836234304138,
+          "pitch": -0.07333644746689671,
+          "rotation": 0,
+          "target": "7-west-runway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-west-runway",
+      "name": "West Runway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.0319798201296333,
+        "pitch": 0.43300696301641217,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0866756142125187,
+          "pitch": -0.08274175044457976,
+          "rotation": 0,
+          "target": "1-turkey-pen-hollow"
+        },
+        {
+          "yaw": -2.9821361155946704,
+          "pitch": -0.085119893241556,
+          "rotation": 0,
+          "target": "0-turkey-pen-south"
+        },
+        {
+          "yaw": -3.0950164142823287,
+          "pitch": -0.08333645609143048,
+          "rotation": 0,
+          "target": "5-runway-view-"
+        },
+        {
+          "yaw": 2.981036166993708,
+          "pitch": -0.07496705237534584,
+          "rotation": 0,
+          "target": "6-runway-grading"
+        },
+        {
+          "yaw": -2.291449155841491,
+          "pitch": -0.0745062681362132,
+          "rotation": 0,
+          "target": "8-work-area"
+        },
+        {
+          "yaw": -2.6120674260406176,
+          "pitch": -0.08779271558256063,
+          "rotation": 0,
+          "target": "2-big-rock-branch"
+        },
+        {
+          "yaw": 2.50104640873744,
+          "pitch": -0.02449943835558166,
+          "rotation": 0,
+          "target": "3-north-of-runway"
+        },
+        {
+          "yaw": 2.800524651750017,
+          "pitch": -0.05162360144433542,
+          "rotation": 0,
+          "target": "4-cripple-creek"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-work-area",
+      "name": "Work Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.3449113674896296,
+        "pitch": 0.6374911013760691,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5757367263967765,
+          "pitch": -0.03590862645697257,
+          "rotation": 0,
+          "target": "7-west-runway"
+        },
+        {
+          "yaw": 0.885293348066396,
+          "pitch": -0.08389458710972164,
+          "rotation": 0,
+          "target": "6-runway-grading"
+        },
+        {
+          "yaw": 1.6088403110792768,
+          "pitch": -0.06948729741314175,
+          "rotation": 0,
+          "target": "3-north-of-runway"
+        },
+        {
+          "yaw": 2.143746796646205,
+          "pitch": -0.06906543151937683,
+          "rotation": 0,
+          "target": "4-cripple-creek"
+        },
+        {
+          "yaw": 2.240606365394921,
+          "pitch": -0.1487192020250223,
+          "rotation": 0,
+          "target": "5-runway-view-"
+        },
+        {
+          "yaw": 2.7758054879101577,
+          "pitch": -0.0748895887470411,
+          "rotation": 0,
+          "target": "1-turkey-pen-hollow"
+        },
+        {
+          "yaw": 2.8981717153948434,
+          "pitch": -0.061477806451653905,
+          "rotation": 0,
+          "target": "0-turkey-pen-south"
+        },
+        {
+          "yaw": -2.7974653279708654,
+          "pitch": -0.06649978245877719,
+          "rotation": 0,
+          "target": "2-big-rock-branch"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Airport 6-7-24",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
